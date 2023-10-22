cff3150d4f2e99a70997a3165635c4e64f06cb9e
Fran-Romar <franromar2002@mail.com>
2023-10-23 00:08:17 +0200

    CSS Update

--- src/components/MovieBanner.jsx
+++ src/components/MovieBanner.jsx
@@ -22,29 +22,31 @@ export default function MovieBanner({
             }}
           >
             {playing ? (
-              <div className="container">
-                <YouTube
-                  videoId={trailer.key}
-                  className="reproductor container"
-                  containerClassName={"youtube-container amru"}
-                  opts={{
-                    width: "100%",
-                    height: "100%",
-                    playerVars: {
-                      autoplay: 1,
-                      controls: 1,
-                      cc_load_policy: 0,
-                      fs: 0,
-                      iv_load_policy: 0,
-                      modestbranding: 0,
-                      rel: 0,
-                      showinfo: 0,
-                    },
-                  }}
-                />
-                <button onClick={() => setPlaying(false)} className="boton">
-                  Close
-                </button>
+              <div className="container-info">
+                <div className="container">
+                  <YouTube
+                    videoId={trailer.key}
+                    className="reproductor container"
+                    containerClassName={"youtube-container amru"}
+                    opts={{
+                      width: "100%",
+                      height: "100%",
+                      playerVars: {
+                        autoplay: 1,
+                        controls: 1,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
+                        rel: 0,
+                        showinfo: 0,
+                      },
+                    }}
+                  />
+                  <button onClick={() => setPlaying(false)} className="boton">
+                    Close
+                  </button>
+                </div>
               </div>
             ) : (
               <BannerInfo
